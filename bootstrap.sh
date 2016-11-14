32165d441b49a066c36db3777a91c3992ef840ea
Jin Lee <jinslee@gmail.com>
2016-11-13 19:14:07 -0800

    just deleting and readding seems to work

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
 ########## Variables
 
 dir=~/dotfiles # dotfiles directory
-olddir=~/dotfiles_old # old dotfiles backup directory
-files="gitconfig gitignore vim/ vimrc" # list of files/folders to symlink in homedir
+files="gitconfig gitignore vim vimrc" # list of files/folders to symlink in homedir
 
 ##########
 
@@ -17,8 +12,9 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+# create symlinks
 for file in $files; do
   echo "Creating symlink to $file in home directory."
+  rm ~/.$file
   ln -s $dir/$file ~/.$file
 done
