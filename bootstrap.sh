7148c3d72e4df92f5d32f6ca8b8ceaab6b86332b
Jin Lee <jinslee@gmail.com>
2016-11-13 19:04:43 -0800

    random fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,11 +12,6 @@ files="gitconfig gitignore vim vimrc" # list of files/folders to symlink in home
 
 ##########
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
@@ -24,8 +19,6 @@ echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
-  echo "Moving any existing dotfiles from ~ to $olddir"
-  mv ~/.$file ~/dotfiles_old/
   echo "Creating symlink to $file in home directory."
   ln -s $dir/$file ~/.$file
 done
